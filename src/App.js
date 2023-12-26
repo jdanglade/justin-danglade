a611075d551bdf56e3f86131d6ca8aa85f81e271
Justin Danglade <jaybirddanglade@gmail.com>
2023-12-26 16:42:01 -0500

    2023 Redisign

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import "./App.css";
-import React, { Suspense, lazy } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
-const Homepage = lazy(() => import("./Pages/Homepage"));
-const Resume = lazy(() => import("./Pages/Resume"));
-const Photos = lazy(() => import("./Pages/Photos"));
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <Router basename="">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/Resume" element={<Resume />} />
-            <Route path="/Photos" element={<Photos />} />
-          </Routes>
-        </Suspense>
-      </Router>
     </div>
   );
 }
